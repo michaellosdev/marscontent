00e6daebabe698b6206294eb5317230ccf653b4c
michaellosdev <mishaloskutov@gmail.com>
2023-09-29 13:40:04 -0700

    form fix

--- lib/sections/Contact/Contact.home.tsx
+++ lib/sections/Contact/Contact.home.tsx
@@ -5,7 +5,8 @@ import { Typography } from "@/lib/components/Typography";
 import Container from "@/lib/components/Container";
 import { AnimateOnView } from "@/lib/utils/AnimateOnView/AnimateOnView";
 import Carousel from "@/lib/components/Carousel";
-import { Card, CardBody, CardHeader } from "@/lib/components/Card";
+import { Card, CardBody, CardHeader, CardTitle } from "@/lib/components/Card";
+import { Button } from "@/lib/components/Button";
 import Form from "@/lib/components/Form";
 import Link from "next/link";
 import Image from "next/image";
@@ -77,7 +78,72 @@ export default function Contact(props: IAppProps) {
         type="fadeIn"
         className="w-full lg:w-1/2 h-full flex items-center justify-center mt-2"
       >
-        <Form />
+        <Card className="lg:w-[80%] w-[100%] h-[42rem]" elevation={2}>
+          <CardHeader className="mb-4">
+            <CardTitle>Contact us</CardTitle>
+          </CardHeader>
+          <CardBody className="h-[90%]  w-full">
+            <form
+              data-netlify="true"
+              name="contact"
+              netlify-honeypot="bot-field"
+              method="POST"
+              className="flex flex-col  w-full h-full"
+            >
+              <input type="hidden" name="form-name" value="contact" />
+              <div className="h-[20%]   ">
+                <label className="sr-only">Name</label>
+                <input
+                  type="text"
+                  className={`px-4 py-2 h-[65%]  rounded-[12px]   text-body border-2   w-full`}
+                  id="fullName"
+                  placeholder="Full Name"
+                  name="fullName"
+                />
+              </div>
+              <div className="h-[20%]">
+                <label className="sr-only">Email</label>
+                <input
+                  type="text"
+                  className={`px-4 py-2 h-[65%]  rounded-[12px]   text-body border-2  w-full`}
+                  id="email"
+                  placeholder="Email"
+                  name="email"
+                />
+              </div>
+              <div className="h-[20%]">
+                <label className="sr-only">Phone</label>
+                <input
+                  type="number"
+                  className={`px-4 py-2 h-[65%]  rounded-[12px]   text-body border-2   w-full`}
+                  id="tel"
+                  placeholder="Phone Number"
+                  name="tel"
+                />
+              </div>
+              <div className="h-[40%]">
+                <label className="sr-only">Message</label>
+                <textarea
+                  placeholder="Message"
+                  className={`px-4 py-2 h-[80%]  rounded-[12px]   text-body border-2  w-full`}
+                  id="message"
+                  name="message"
+                />
+              </div>
+              <div className="w-full flex justify-end ">
+                <Button
+                  variant="filled"
+                  size="lg"
+                  className=""
+                  elevation={4}
+                  type="submit"
+                >
+                  Submit
+                </Button>
+              </div>
+            </form>
+          </CardBody>
+        </Card>
       </AnimateOnView>
     </div>
   );
